bf735cd693188378941537b26b9c245c37ae2224
Трофимов Демьян <SandY898@stud.vvsu.ru>
2023-10-06 16:24:35 +1000

    vers with tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import Reg from "./Componets/Reg";
+import Main from "./Pages/Main";
+import ErrorPage from "./Pages/ErrorPage"
+import { BrowserRouter } from "react-router-dom";
+import { Routes } from "react-router-dom";
+import { Route } from "react-router-dom";
+import UIButton from "./Componets/UIButton";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/reg" element={<Reg />} />
+        <Route path="/" element={<Main />} />
+        <Route path="/*" element={<ErrorPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
